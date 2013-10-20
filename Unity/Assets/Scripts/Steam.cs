1cb1965d69fb8d498b139d9e7ab9d7872264001a
Joe Howse <jhowse@ad-dispatch.com>
2013-10-20 11:56:17 -0300

    Steam collision delay

--- Unity/Assets/Scripts/Steam.cs
+++ Unity/Assets/Scripts/Steam.cs
@@ -5,12 +5,19 @@ using System.Collections;
 [RequireComponent (typeof(ParticleSystem))]
 public class Steam : MonoBehaviour {
 	
+	public float toggleColliderDelay = 0.5f;
+	
 	void Awake() {
 		particleSystem.enableEmission = collider.enabled = false;
 	}
 	
 	public void Toggle() {
-		particleSystem.enableEmission = collider.enabled =
-				!collider.enabled;
+		particleSystem.enableEmission = !particleSystem.enableEmission;
+		StartCoroutine(ToggleCollider());
+	}
+	
+	IEnumerator ToggleCollider() {
+		yield return new WaitForSeconds(toggleColliderDelay);
+		collider.enabled = !collider.enabled;
 	}
 }
